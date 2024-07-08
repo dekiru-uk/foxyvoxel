844334156e9783838e6cc4740bc0cc69a50ee515
Jake Smith <jake74@gmail.com>
2024-07-09 00:40:11 +0100

    remove unused flexible pages

--- template-parts/flexible-intro.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $intro = get_sub_field( 'intro' );
-?>
-
-<div class="section intro">
-    <h2 class="project-intro"><?php echo $intro; ?></h2>
-</div>
-
--- template-parts/flexible-two_up.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $image_1 = get_sub_field( 'image_1' );
-    $image_2 = get_sub_field( 'image_2' );
-?>
-
-<div class="section two-up-image">
-    <img src="<?php echo $image_1['sizes']['large']; ?>" alt="<?php echo $image_1['alt']; ?>">
-    <img src="<?php echo $image_2['sizes']['large']; ?>" alt="<?php echo $image_2['alt']; ?>">
-</div>
