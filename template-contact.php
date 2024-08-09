6d31e9d938464fa11da9db02ac4dee656d83d426
Jake Smith <jake74@gmail.com>
2024-08-09 15:39:14 +0100

    Update game single page layout to include news section

--- template-contact.php
+++ template-contact.php
@@ -7,6 +7,9 @@
 
 get_header();
 
+$image = get_field('image');
+$connect_text = get_field('connect_text');
+
 $intro = get_field('intro');
 $form = get_field('form');
 $form_id = $form->ID;
@@ -17,11 +20,23 @@ $form_id = $form->ID;
 		<main id="main" class="site-main contact-us">
 			<div class="section-content">
 				<div class="image-block">
+					<img src="<?php echo $image['url']; ?>"
+						alt="<?php echo $image['alt']; ?>"
+						width="<?php echo $image['width']; ?>" height="<?php echo $image['height']; ?>"
+						loading="lazy"
+					>
+				</div>
+
+				<div class="connect">
+					<p class="connect-title"><?php echo $connect_text; ?></p>
+					<?php get_template_part( 'template-parts/partial', 'social' ); ?>
 				</div>
 
 				<div class="form-block">
 					<h1 class="section-title"><?php the_title(); ?></h1>
-					<?php echo $intro; ?>
+					<div class="intro-copy">
+						<?php echo $intro; ?>
+					</div>
 					<?php // [forminator_form id="196"] 
 						echo do_shortcode( '[forminator_form id="' . $form_id . '"]' );
 					?>
