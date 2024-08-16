b45a8510a88fb71710ca3a555bcea6c615258a46
Jake Smith <jake74@gmail.com>
2024-08-16 14:27:26 +0100

    searchform and colur news

--- index.php
+++ index.php
@@ -159,14 +159,7 @@ $third_id = null;
 							<p class="subtitle">Other news</p>
 
 							<div class="search-form">
-								<?php // get_search_form(); ?>
-
-								<form action="/" method="get" class="searchform">
-									<input type="hidden" value="post" name="post_type" id="post_type" />
-									<label for="search">Search</label>
-									<input type="text" name="s" placeholder="Enter your search here" id="search" value="<?php the_search_query(); ?>" />
-									<button class="fv-button fv-button-short">Search</button>
-								</form>
+								<?php get_search_form(); ?>
 							</div>
 						</div>
 					<?php endif; ?>
---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form action="/" method="get" class="searchform">
+	<input type="hidden" value="post" name="post_type" id="post_type" />
+	<label for="search">Search</label>
+	<input type="text" name="s" placeholder="Enter your search here" id="search" value="<?php the_search_query(); ?>" />
+	<button class="fv-button fv-button-short">Search</button>
+</form>
