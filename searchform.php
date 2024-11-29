f1b2e3a8033200789caea0fca2836722ccb95dee
Jake Smith <jake@dekiru.uk>
2024-11-29 15:40:20 +0000

    Update search form action URL to use site URL for improved functionality

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form action="/" method="get" class="searchform">
+<form action="<?php echo $site_url; ?>" method="get" class="searchform">
 	<input type="hidden" value="post" name="post_type" id="post_type" />
 	<label for="search">Search</label>
 	<input type="text" name="s" placeholder="Search…" id="search" value="<?php the_search_query(); ?>" />
